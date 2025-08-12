8b751df11a0369142fa1d41e99526f0701fab7ed
Rohit kumar <128993662+rohitlkr7@users.noreply.github.com>
2025-08-12 09:08:14 +0530

    dsa added

---
+++ part-3/valid-voter/script.js
@@ -0,0 +1,17 @@
+// valid voter_
+
+let age = Number(prompt("what is your age"));
+
+if (isNaN(age)) {
+  if (age >= 18) {
+    console.log("go for vote");
+  } else {
+    console.log("you can not vote");
+  }
+}
+
+
+
+
+
+// Bijali BILL
