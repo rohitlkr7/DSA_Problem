501f51c8cf51988132eb340b05e836d183b3f520
Rohit kumar <128993662+rohitlkr7@users.noreply.github.com>
2025-08-14 00:50:00 +0530

    file added

---
+++ part-3/Loop/script.js
@@ -0,0 +1,18 @@
+// for-loop
+
+// for(start; end; Channe){
+
+// }
+
+// print 1 to 10
+
+// for(var i=1; i<=10; i++){
+//     console.log("hello world " + i)
+// }
+
+
+// reverse number
+for(var i=10; i>0; i-- ){
+    console.log(i)
+}
+
---
+++ part-3/sum-of-num/index.js
@@ -0,0 +1,7 @@
+let n = Number(prompt("enter some number"));
+
+let sum = 0;
+for (let i = 1; i <= n; i++) {
+  sum += i;
+}
+console.log(sum);
