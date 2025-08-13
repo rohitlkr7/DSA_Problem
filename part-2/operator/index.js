f0dc5f53d45285212ec2c789abcf84b30f1b6f10
Rohit kumar <128993662+rohitlkr7@users.noreply.github.com>
2025-08-13 23:41:46 +0530

    new file added

---
+++ part-2/operator/index.js
@@ -0,0 +1,6 @@
+// ternary operator
+1232 > 123 ? console.log("big hai ") : console.log("small hai")
+
+// nested ternary operator
+let num = 10;
+console.log(num > 0 ? "big number":num<0? "small number" : "zero")
---
+++ part-2/switch-case/script.js
@@ -0,0 +1,18 @@
+let day = 4;
+
+switch (day) {
+  case 1:
+    console.log("monday");
+    break;
+
+  case 2:
+    console.log("Tuesday");
+    break;
+
+  case 3:
+    console.log("wednesday");
+    break;
+
+  default:
+    console.log("invalid");
+}
